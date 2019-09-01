260e9f886f2371d6914abd2c7f71574f75104ddf
Guillermo Banchero <gbanchero12@gmail.com>
2019-09-01 13:20:39 -0300

    img added

--- src/app/services/statistics.service.ts
+++ src/app/services/statistics.service.ts
@@ -30,30 +30,10 @@ export class StatisticsService {
     Object.keys(statisticsObject).forEach(key => {
       const statistic = statisticsObject[key];
       statistics.push(statistic);
+      console.log(statistics);
     });
 
     return statistics[1];
-
-    /*statistics = statistics[1] as StatisticsModel[];
-
-    const joinedStaditics: StatisticsModel[] = [];
-    joinedStaditics.push(statistics[0]);
-    for (let i = 0; i <= statistics.length; i++) {
-      const actual = statistics[i];
-
-      // tslint:disable-next-line: prefer-for-of
-      for (let j = 0 ; j <= joinedStaditics.length; j++) {
-        const actualJoined = joinedStaditics[j];
-        if (actual.PLAYER === actualJoined.PLAYER) {
-          actualJoined.SCORE++;
-
-        } else {
-          joinedStaditics.push(actual);
-        }
-      }
-    }
-
-    return joinedStaditics;*/
   }
 
 }
